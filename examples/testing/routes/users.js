e39fa62161f63fa7d4aaef0361dbf696ff1e05c2
Huyen Pham <huyen.pham@asnet.com.vn>
2025-06-15 22:49:14 +0700

    chore: setup testing

---
+++ examples/testing/routes/users.js
@@ -0,0 +1,33 @@
+module.exports = (app) => {
+  const Users = app.db.models.users;
+
+  app
+    .route("/user")
+    .all(app.auth.authenticate())
+    .get((req, res) => {
+      Users.findById(req.user.id, {
+        attributes: ["id", "name", "email"],
+      })
+        .then((result) => res.json(result))
+        .catch((error) => {
+          res.status(412).json({ message: error.message });
+        });
+    })
+    .delete((req, res) => {
+      Users.destroy({
+        where: { id: req.user.id },
+      })
+        .then(() => res.sendStatus(204))
+        .catch((error) => {
+          res.status(412).json({ message: error.message });
+        });
+    });
+
+  app.post("/users", (req, res) => {
+    Users.create(req.body)
+      .then((result) => res.json(result))
+      .catch((error) => {
+        res.status(412).json({ message: error.message });
+      });
+  });
+};
