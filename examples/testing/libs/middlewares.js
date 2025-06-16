f65c2a8a92fa6fa3c65e1130547cfeda1ac578e5
Huyen Pham <huyen.pham@asnet.com.vn>
2025-06-16 18:00:33 +0700

    feat: create api doc and using logger

--- examples/testing/libs/middlewares.js
+++ examples/testing/libs/middlewares.js
@@ -1,8 +1,26 @@
 const bodyParser = require("body-parser");
+const express = require("express");
+const cors = require("cors");
+const morgan = require("morgan");
+const logger = require("./logger.js");
 
 module.exports = (app) => {
   app.set("port", 3001);
   app.set("json spaces", 4);
+  app.use(
+    morgan("common", {
+      stream: {
+        write: (message) => logger.info(message),
+      },
+    })
+  );
+  app.use(
+    cors({
+      origin: ["http://localhost:3000"],
+      methods: ["GET", "POST", "PUT", "DELETE"],
+      allowedHeaders: ["Content-Type", "Authorization"],
+    })
+  );
   app.use(bodyParser.json());
   app.use(app.auth.initialize());
   app.use((req, res, next) => {
@@ -11,4 +29,5 @@ module.exports = (app) => {
     }
     next();
   });
+  app.use(express.static("public"));
 };
--- examples/testing/routes/token.js
+++ examples/testing/routes/token.js
@@ -4,6 +4,25 @@ module.exports = (app) => {
   const Users = app.db.models.users;
   const config = app.libs.config;
 
+  /**
+   * @api {post} /token Authentication Token
+   * @apiGroup Credentials
+   * @apiParam {String} email User's email
+   * @apiParam {String} password User's password
+   * @apiParamExample {json} Request-Example:
+   * {
+   *   "email": "test@example.com",
+   *   "password": "123456"
+   * }
+   * @apiSuccess {String} token User's token
+   * @apiSuccessExample {json} Success-Response:
+   *     HTTP/1.1 200 OK
+   *     {
+   *       "token": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwiaWF0IjoxNTQ4NjE2MjI4fQ.2vKZ7a9H2lUw3Mq4fK5a6g7b8c9d0e1f"
+   *     }
+   * @apiErrorExample {json} Error-Response:
+   *     HTTP/1.1 401 Unauthorized
+   */
   app.post("/token", async (req, res) => {
     const { email, password } = req.body;
 
