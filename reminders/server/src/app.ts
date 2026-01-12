d3c6b800fccbe656d30007000bf9585eab5f4cb3
Huyen Pham <huyen.pham@asnet.com.vn>
2026-01-12 17:38:20 +0700

    chore: init folder app

--- reminders/server/src/app.ts
+++ reminders/server/src/app.ts
@@ -2,6 +2,7 @@ import express, { Application, Request, Response } from 'express';
 import cors from 'cors';
 import helmet from 'helmet';
 import dotenv from 'dotenv';
+import 'reflect-metadata';
 
 dotenv.config();
 
